f9eeda83ae7e65913ed24ec058dad292cc947258
Craig Barry <craigfbarry@gmail.com>
2020-06-28 12:02:40 +0930

    Added service worker file and webpack config.

---
+++ public/webpack.config.js
@@ -0,0 +1,14 @@
+
+
+const config = {
+    entry: "./public/assets/js/app.js",
+    output: {
+      path: __dirname + "/public/dist",
+      filename: "bundle.js"
+    },
+    mode: "development"
+};
+
+
+
+module.exports = config;
