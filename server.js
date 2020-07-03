eac214c9ea87ebe0b281c7224b094cf5f3920863
Craig Barry <craigfbarry@gmail.com>
2020-07-03 11:13:09 +0930

    Troubleshooting Heroku.

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+const MONGODB_URI = process.env.MONGODB_URI||"mongodb://localhost/budget";
 
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
